e83aeaa5e18a8704b1df03e883727c8d798a2d4c
github-action-benchmark <github@users.noreply.github.com>
2021-05-30 07:55:49 +0000

    add Benchmark (cargo) benchmark result for aa0aa2036ee17febc2b21135debdf6a9679f48ae

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1622358050736,
+  "lastUpdate": 1622361349639,
   "repoUrl": "https://github.com/tsg-ut/bf",
   "entries": {
     "Benchmark": [
@@ -35,6 +35,42 @@ window.BENCHMARK_DATA = {
             "name": "elvm_hello",
             "value": 1029499366,
             "range": "± 19282491",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "22975590+n4o847@users.noreply.github.com",
+            "name": "n4o847",
+            "username": "n4o847"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aa0aa2036ee17febc2b21135debdf6a9679f48ae",
+          "message": "Merge pull request #10 from tsg-ut/develop\n\nMerge develop into master",
+          "timestamp": "2021-05-30T16:52:59+09:00",
+          "tree_id": "9f274c3443ff6ae8e8a11d2fd4ddb3a91f7a3194",
+          "url": "https://github.com/tsg-ut/bf/commit/aa0aa2036ee17febc2b21135debdf6a9679f48ae"
+        },
+        "date": 1622361348651,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cat",
+            "value": 428,
+            "range": "± 24",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "elvm_hello",
+            "value": 1103670889,
+            "range": "± 57329615",
             "unit": "ns/iter"
           }
         ]
