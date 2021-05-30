c6d1dc1fd88887605543e48ea8a52c06f95e8dc1
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-05-30 16:31:46 +0900

    Use `eprintln!` and `exit` instead of `panic!`

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,27 @@ use bf::compile;
 use std::{
     env, fs,
     io::{self, stdout, Read, Write},
+    process::exit,
 };
 
 fn main() -> io::Result<()> {
     let args: Vec<_> = env::args().collect();
     if let Some(filename) = args.get(1) {
         let code = fs::read_to_string(filename).unwrap_or_else(|_| {
-            panic!("cannnot read file `{}'", filename);
+            eprintln!("cannot read file '{}'", filename);
+            exit(1);
         });
-        let program = compile(&code).unwrap();
+        let program = compile(&code).unwrap_or_else(|e| {
+            eprintln!("compile error: {}", e);
+            exit(1);
+        });
         let mut input = Vec::new();
         io::stdin().read_to_end(&mut input)?;
         let output = program.run(&input).unwrap();
         stdout().write_all(&output)?;
     } else {
         eprintln!("filename not specified");
+        exit(1);
     }
     Ok(())
 }
