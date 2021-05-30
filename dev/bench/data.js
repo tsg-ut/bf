372e9a53a96a039b40c6cfbc49e1327fae75850e
github-action-benchmark <github@users.noreply.github.com>
2021-05-30 07:00:50 +0000

    add Benchmark (cargo) benchmark result for 85f004a36715067e34bad821be715a96b3f57d56

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1622358050736,
+  "repoUrl": "https://github.com/tsg-ut/bf",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "22975590+n4o847@users.noreply.github.com",
+            "name": "n4o847",
+            "username": "n4o847"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "85f004a36715067e34bad821be715a96b3f57d56",
+          "message": "Merge pull request #6 from tsg-ut/fix-bench\n\nFix benchmarks",
+          "timestamp": "2021-05-30T15:37:56+09:00",
+          "tree_id": "07bb0313abd5afff6e136970585bfae670c2fc26",
+          "url": "https://github.com/tsg-ut/bf/commit/85f004a36715067e34bad821be715a96b3f57d56"
+        },
+        "date": 1622358050182,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "cat",
+            "value": 403,
+            "range": "± 10",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "elvm_hello",
+            "value": 1029499366,
+            "range": "± 19282491",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
