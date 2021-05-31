b9c84c9ae413f5bdf4b1654649e448e6e53a4166
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-05-31 21:00:40 +0900

    Add tests

--- tests/test.rs
+++ tests/test.rs
@@ -12,3 +12,18 @@ fn test_unmatched_close() {
     let code = fs::read_to_string("examples/unmatched-close.bf").unwrap();
     assert!(compile(&code).is_err());
 }
+
+#[test]
+fn test_cat() {
+    let code = fs::read_to_string("examples/cat.bf").unwrap();
+    let program = compile(&code).unwrap();
+    assert_eq!(program.run(b"meow"), Some(b"meow".to_vec()));
+}
+
+#[test]
+fn test_hello() {
+    // Note: hello.bf needs negative memory addresses
+    let code = fs::read_to_string("examples/hello.bf").unwrap();
+    let program = compile(&code).unwrap();
+    assert_eq!(program.run(b""), Some(b"Hello, World!".to_vec()));
+}
